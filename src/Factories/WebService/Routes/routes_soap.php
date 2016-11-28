855f487bb8666ed8d72e27aca714eec7bef5b429
Josh Harington <vamoose@Joshs-MacBook-Pro.local>
2016-11-28 09:48:46 +0200

    Started on SOAP

--- src/Factories/WebService/Routes/routes_soap.php
+++ src/Factories/WebService/Routes/routes_soap.php
@@ -3,10 +3,10 @@
  * Created by PhpStorm.
  * User: vamoose
  * Date: 2016/11/28
- * Time: 9:40 AM
+ * Time: 9:47 AM
  */
 
-Route::group(['namespace' => 'EONConsulting\PHPStencil\src\Factories\WebService\REST\Controllers', 'prefix' => '/_eon_phpstencil/api/rest'], function() {
+Route::group(['namespace' => 'EONConsulting\PHPStencil\src\Factories\WebService\SOAP\Controllers', 'prefix' => '/_eon_phpstencil/api/soap'], function() {
     Route::get('/{id}', ['as' => 'phpstencil.rest.get', 'uses' => 'RestController@get']);
     Route::put('/', ['as' => 'phpstencil.rest.put', 'uses' => 'RestController@put']);
     Route::post('/{id}', ['as' => 'phpstencil.rest.post', 'uses' => 'RestController@post']);
