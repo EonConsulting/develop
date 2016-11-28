49d7e59fd693eb8da0f99baacf585506a468e734
Josh Harington <vamoose@Joshs-MacBook-Pro.local>
2016-11-28 09:00:05 +0200

    Init

---
+++ src/Http/helpers.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vamoose
+ * Date: 2016/11/28
+ * Time: 8:49 AM
+ */
---
+++ src/PHPStencil.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vamoose
+ * Date: 2016/11/28
+ * Time: 8:50 AM
+ */
+
+namespace EONConsulting\PHPStencil;
+
+
+class PHPStencil {
+
+}
