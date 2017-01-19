d1e1682a0c544c177c839095b837b161ee2ea35d
Josh <jharing10@Joshs-MacBook-Pro.local>
2017-01-19 09:28:01 +0200

    Added Routes folder

---
+++ src/Routes/web.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jharing10
+ * Date: 2017/01/19
+ * Time: 9:27 AM
+ */
--- src/StorylineCoreServiceProvider.php
+++ src/StorylineCoreServiceProvider.php
@@ -17,11 +17,19 @@ class StorylineCoreServiceProvider extends ServiceProvider {
         $this->app->singleton( 'storyline_core', function () {
             return new StorylineCore;
         });
-
     }
 
     public function boot() {
-
+//        $this->loadRoutesFrom(__DIR__.'/Routes/routes.php');
+//        $this->publishes([
+//            __DIR__ . '/database/migrations' => $this->app->databasePath() . '/migrations'
+//        ], 'migrations');
+//        $this->publishes([
+//            __DIR__ . '/database/seeds' => $this->app->databasePath() . '/seeds'
+//        ], 'seeders');
+//        $this->publishes([
+//            __DIR__ . '/resources/views' => base_path('resources/views'),
+//        ]);
     }
 
 }
