1055852a85ca3f89f6a4a86b3a7c78778dfdb3d2
Josh <jharing10@Joshs-MacBook-Pro.local>
2017-03-08 13:30:18 +0200

    Updates

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -4,7 +4,7 @@ Route::group(['middleware' => ['web'], 'prefix' => '/eon/lti', 'namespace' => 'E
     Route::group(['middleware' => ['auth']], function() {
 
         // list all of the api's
-        Route::get('/appstore', ['as' => 'eon.laravellti.appstore', 'uses' => 'AppStoreController@index']);
+        Route::match(['get', 'post'], '/appstore', ['as' => 'eon.laravellti.appstore', 'uses' => 'AppStoreController@index']);
         Route::any('/appstore/launch/{context}', ['as' => 'eon.laravellti.appstore.launch', 'uses' => 'AppStoreController@launch']);
     });
 });
