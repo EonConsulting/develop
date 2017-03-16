8cdb3abb16ffc5b33555fbf5d80ae4caf68748ee
Peace Ngara <peacengara@aol.com>
2017-03-16 17:58:14 +0200

    Updates
    Updates

--- src/resources/views/editorv2.blade.php
+++ src/resources/views/editorv2.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8">
+            <div>
+                <div id ="editor-wrapper">
                 <form name="save-data" id="save-data" action="/cksavedata">
                 <textarea id="ltieditorv2inst" class="ckeditor">&lt;p&gt;Initial editor contents.&lt;/p&gt;</textarea>
                     <br />
-                    <button id="submit" class="btn" name="submit">Save Data</button>
+                    <button id="submit" class="btn unisa-blue-btn" name="submit">Save Data</button>
                 </form>
+                </div>
                 <script>
                     /**
                      * Create a New Instance of the CK Editor
@@ -19,7 +19,16 @@
                             extraPlugins: 'ltieditorv2'
                         },
                             config.allowedContent = true
-                        )
+                        );
+
+                        CKEDITOR.on('instanceReady',
+                        function (evt)
+                        {
+                            var editor = evt.editor;
+                            //editor.execCommand('maximize');
+                            editor.resize("100%", $('#editor-wrapper').height());
+
+                        });
 
                         CKEDITOR.document.appendStyleSheet("{{URL::asset('/vendor/ckeditorpluginv2/css/custom-contents.css')}}");
 //                        CKEDITOR.config.allowedContent = true;
@@ -39,7 +48,8 @@
                                 },
                                 success: function (response) {
 //                                    alert(data); // Get Current Editor Data.
-//                                    console.log(response);
+                                    console.log(response);
+                                    $('#ltieditorv2inst').append(response) ;
                                 }
 
                             });
@@ -49,10 +59,6 @@
                 </script>
             </div>
 
-        </div>
-
-
-    </div>
 
 
 @endsection
