bf9ca5c8b47f1042f07a5cc4f73c0f34a2702a12
Peace Ngara <peacengara@aol.com>
2017-07-19 13:41:35 +0200

    'Updates'

--- src/StorylineBreadcrumbs.php
+++ src/StorylineBreadcrumbs.php
@@ -7,11 +7,16 @@
  */
 
 namespace EONConsulting\Storyline\Breadcrumbs;
-
+use App\Models\StorylineItem;
+use EONConsulting\Storyline\Breadcrumbs\Classes\ParentKeysIterator;
+use Illuminate\Support\Facades\DB;
+use App\Models\Course;
 
 class StorylineBreadcrumbs {
 
     protected $found = false;
+    protected $next_id;
+    Protected $next_found = false;
 
     // data requires the following structure:
     //  [
@@ -72,6 +77,25 @@ class StorylineBreadcrumbs {
         return $breadcrumbs;
     }
 
+    //Get Next using DB taxonomy :: Peace Implementation
+    public function getNextStoryLineItem($id, StorylineItem $storylineItem)
+    {
+
+         $nextStorylineItem = $storylineItem->next($id);
+
+
+        return $nextStorylineItem;
+    }
+    public function getPrevStoryLineItem($id, StorylineItem $storylineItem)
+    {
+
+         $prevStorylineItem = $storylineItem->previous($id);
+
+
+        return $prevStorylineItem;
+    }
+
+
     public function getBreadcrumbs($config, $parent_config = false) {
         $data = storyline_core()->prepareForBreadcrumbs();
         if(!$parent_config)
@@ -105,5 +129,50 @@ class StorylineBreadcrumbs {
             '/vendor/storyline/breadcrumbs/css/style.css'
         ];
     }
+
+    public function getCategoryTreeIDs($catID) {
+
+        $html = '';
+//       $row = DB::getInstance()->query("SELECT parent FROM categories WHERE ID = '$catID'")->fetch();
+        //$row = DB::table('storyline_items')->select('parent_id')->where('id', $catID)->first();
+        $row = array();
+        $row= StorylineItem::where('id', $catID)->select('parent_id')->first()->toArray();
+        $path = array();
+        if (!$row['parent_id'] == '') {
+            $path[] = $row['parent_id'];
+            $path = array_merge($this->getCategoryTreeIDs($row['parent_id']), $path);
+        }
+        return $path;
+
+    }
+
+    public function showCatBreadCrumb($catID, $data=[], $course) {
+        $d = $data;
+        $base = route('lti.courses.single.lectures.item', [$course->id, $d['id']]);
+        $array = $this->getCategoryTreeIDs($catID);
+        $html = '';
+        //$html .= '<ul class="breadcrumbs">';
+        $numItems = count($array);
+        for ($i = 0; $i<=$numItems-1; $i++) {
+            //$html .= '<li><a href="' . $base . '"></a></li>';
+            $html .= '<li>' . $this->getNameLink($array[$i], $data, $course) . '&raquo' . '</li>';
+
+
+        }
+        //$html .= '</ul>';
+        return $html;
+    }
+
+    public function getNameLink($catID, $data=[], $course) {
+        $catID = (int) $catID;
+        $d = $data;
+        //$check = DB::getInstance()->query("SELECT ID, Name FROM categories WHERE ID = '$catID' LIMIT 1")->fetch();
+//        $check = DB::table('storyline_items')->select('id, name')->where('id', $catID)->first();
+        $check= StorylineItem::where('id', $catID)->select('id','name', 'file_url')->first()->toArray();
+        $html = '<a href=' . route('lti.courses.single.lectures.item', [$course->id, $d['id']]) .'> '. $check['name'] .' </a>';
+        return $html;
+
+    }
+
 
 }
