18e020228e4065b11f34135112e8f76fb3b6987a
reggiestain <reggiestain@gmail.com>
2017-07-20 12:59:18 +0200

    TagCloud Component update 0.01

--- src/StorylineTagCloud.php
+++ src/StorylineTagCloud.php
@@ -10,27 +10,46 @@ namespace EONConsulting\Storyline\TagCloud;
 
 
 class StorylineTagCloud {
-
+    /**
+     * @return array
+     */
     public function getStyles() {
         return ['/vendor/storyline/tagcloud/jqcloud.css'];
     }
 
+    /**
+     * @return array
+     */
     public function getScripts() {
         return ['/vendor/storyline/tagcloud/jqcloud-1.0.4.js'];
     }
 
+    /**
+     * @return string
+     */
     public function getCustomStyles() {
         return "div.jqcloud span.vertical { -webkit-writing-mode: vertical-rl; writing-mode: tb-rl; }";
     }
 
+    /**
+     * @param $word_list
+     * @return string
+     */
     public function getCustomScripts($word_list) {
         return "$('#tag-cloud').jQCloud(" . json_encode($word_list) . ");";
     }
 
+    /**
+     * @return string
+     */
     public function getHTML() {
         return '<div id="tag-cloud" style="width: 100%; height: 350px;"></div>';
     }
 
+    /**
+     * @param $word_list
+     * @return array
+     */
     public function generateWordList($word_list) {
 
         $return = [];
