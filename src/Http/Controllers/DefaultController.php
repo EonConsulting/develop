3af3525e07d562f2f1d4e1c452be2b1621867373
reggiestain <reggiestain@gmail.com>
2017-07-26 17:34:47 +0200

    updated controller with storyline save content

--- src/Http/Controllers/DefaultController.php
+++ src/Http/Controllers/DefaultController.php
@@ -82,4 +82,41 @@ class DefaultController extends LTIBaseController {
         $item->buildTree($CVS);
     }
 
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function storeContent(Request $request){
+        if($request->ajax()) {
+
+            if($request->get('file_name')) {
+                $page = public_path() . '/EON/system/public/vendor/storyline/core/files/content/' . $this->new_file_request($request) . '.html';
+                $file = fopen($page, "w");
+                fwrite($file, json_decode($request->get('data')));
+                fclose($file);
+                $results_array =    ['message'=>'success','data'=>$page];
+                echo json_encode($results_array);
+            }else{
+                $results_array = ['message'=>'error'];
+                echo json_encode($results_array);
+            }
+
+        }
+    }
+
+    /**
+     * @param $request
+     * @return mixed
+     *
+     */
+    protected function new_file_request($request) {
+        $file_name = $request->get('file_name');
+        if (str_word_count($file_name) > 1) {
+            $new_file_name = preg_replace('/\s+/', '_', $file_name);
+            return $new_file_name;
+        } else {
+            return $file_name;
+        }
+    }
+
 }
--- src/Http/routes.php
+++ src/Http/routes.php
@@ -4,5 +4,6 @@ Route::group(['middleware' => ['web'],'prefix' => '/lecturer'], function() {
 
 Route::get('/csv/fileupload/{course}/{filetype}','EONConsulting\Storyline\Table\Http\Controllers\DefaultController@getFileuploadView');
 Route::match(['get','post'],'/csv/storeStoryline/{course}','EONConsulting\Storyline\Table\Http\Controllers\DefaultController@storeStoryline');
+Route::match(['post'],'storeContent','EONConsulting\Storyline\Table\Http\Controllers\DefaultController@storeContent');
 
 });
