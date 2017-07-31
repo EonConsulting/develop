bf81599577f8a47603e33d355d1aeea174cf1254
reggiestain <reggiestain@gmail.com>
2017-07-31 10:46:37 +0200

    Updated routes.php

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['middleware' => ['web'],'prefix' => '/lecturer'], function() {
+Route::group(['middleware' => ['web'],'prefix' => '/e-content/lecturer'], function() {
 
 Route::get('/csv/fileupload/{course}/{filetype}','EONConsulting\Storyline\Table\Http\Controllers\DefaultController@getFileuploadView');
 Route::match(['get','post'],'/csv/storeStoryline/{course}','EONConsulting\Storyline\Table\Http\Controllers\DefaultController@storeStoryline');
