f77084864d5481bff8925c3b6a0398d998fbc28a
Kenny Crous <kcrous@iqbusiness.net>
2017-08-08 10:07:33 +0200

    Squashed '.vim/bundle/app-store/' content from commit 22bf289
    git-subtree-dir: .vim/bundle/app-store
    git-subtree-split: 22bf289fe55259e7f34e3c70fdce504435d52984

---
+++ src/functions.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jharing10
+ * Date: 2017/02/12
+ * Time: 12:27 AM
+ */
+if(!function_exists('app_store')) {
+    function app_store() {
+        return app('app_store');
+    }
+}
