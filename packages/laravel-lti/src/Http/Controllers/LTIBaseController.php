0a5ffeed54951f4ed83627f3f98ba84a3b98acf9
Michael Hanekom <michael@vertopia.co.za>
2017-08-15 10:11:32 +0200

    Included tsugi-lib repo into laravel-lti/vendor/tsugi/lib Commented use of NULL class name in LTIBaseController.php

--- packages/laravel-lti/src/Http/Controllers/LTIBaseController.php
+++ packages/laravel-lti/src/Http/Controllers/LTIBaseController.php
@@ -14,11 +14,10 @@ use App\Models\User;
 use EONConsulting\LaravelLTI\Models\UserLTILink;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Symfony\Component\Validator\Constraints\Null;
+//use Symfony\Component\Validator\Constraints\Null;
 use Tsugi\Config\ConfigInfo;
 use Tsugi\Laravel\LTIX;
 use Illuminate\Support\Facades\Hash;
-
 use App\Http\Controllers\Controller;
 use Tsugi\Util\LTI;
 
