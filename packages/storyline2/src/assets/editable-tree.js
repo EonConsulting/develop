a2e73b828c2bdada7b1817bc257fec27af5f18c5
Kenny Crous <kcrous@iqbusiness.net>
2017-09-04 15:51:59 +0200

    made some changes to tree

--- packages/storyline2/src/assets/editable-tree.js
+++ packages/storyline2/src/assets/editable-tree.js
@@ -14,7 +14,7 @@ $.getJSON(url,
 function renderTree(tree_data) {
     console.log(tree_data);
 
-    $("#tree").jstree({
+    $(tree_id).jstree({
         "core" : {
             "animation" : 0,
             "check_callback" : true,
@@ -29,30 +29,33 @@ function renderTree(tree_data) {
 function treeToJSON(){
 
     var v = $('#tree').jstree(true).get_json();
-    //var mytext = JSON.stringify(v);
     console.log(v);
 
 }
 
+
+//detect when node is clicked, ie. selected node changes
+$(tree_id).on("changed.jstree", function (e, data) {
+
+    console.log("Node selected");
+
+});
+
+
+//detect changes in the tree structure
 $events = [
+
     "create_node.jstree",
     "rename_node.jstree",
     "delete_node.jstree",
     "move_node.jstree",
     "cut.jstree",
     "paste.jstree"
+
 ];
 
-/*$('#tree').on("changed.jstree", function (e, data) {
-    treeToJSON();
-});*/
-
-/*$(".menu_collapse").each(function( index ) {
-    $(this).addClass("hidden", 1000);
-});*/
-
 $events.each(function(item){
-    $('#tree').on(item, function (e, data) {
+    $(tree_id).on(item, function (e, data) {
         treeToJSON();
     });
 });
