649790e96bd32385463eebd0f2341e0e76cbb39b
Michael Hanekom <michael@vertopia.co.za>
2017-09-15 17:38:11 +0200

    Fixed reggie routing

--- packages/student-progression/src/routes.php
+++ packages/student-progression/src/routes.php
@@ -1,9 +1,10 @@
 <?php
 
-Route::group(['middleware' => ['web'],'prefix' => 'student', 'namespace' => 'EONConsulting\Student\Progression\Http\Controllers'], function() {
+Route::group(['middleware' => ['web'], 'prefix' => 'student', 'namespace' => 'EONConsulting\Student\Progression\Http\Controllers'], function() {
+    Route::group(['middleware' => ['auth']], function() {
 
-//Route::get('/csv/fileupload/{course}/{filetype}','EONConsulting\Storyline\Table\Http\Controllers\DefaultController@getFileuploadView');
-Route::post('/progression',['as' => 'student.progression', 'uses' => 'DefaultController@storeProgress']);
-//Route::match(['post'],'/storeContent','EONConsulting\Storyline\Table\Http\Controllers\DefaultController@storeContent');
-
+        //Route::get('/csv/fileupload/{course}/{filetype}','EONConsulting\Storyline\Table\Http\Controllers\DefaultController@getFileuploadView');
+        Route::post('/progression', ['as' => 'student.progression', 'uses' => 'DefaultController@storeProgress']);
+        //Route::match(['post'],'/storeContent','EONConsulting\Storyline\Table\Http\Controllers\DefaultController@storeContent');
+    });
 });
