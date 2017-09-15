537f2d9284240954713b6b23a1c4b808247383b2
Michael Hanekom <michael@vertopia.co.za>
2017-09-15 17:25:28 +0200

    Updates to routing

--- packages/student-progression/src/routes.php
+++ packages/student-progression/src/routes.php
@@ -3,7 +3,7 @@
 Route::group(['middleware' => ['web'],'prefix' => 'student', 'namespace' => 'EONConsulting\Student\Progression\Http\Controllers'], function() {
 
 //Route::get('/csv/fileupload/{course}/{filetype}','EONConsulting\Storyline\Table\Http\Controllers\DefaultController@getFileuploadView');
-Route::post('/progression','DefaultController@storeProgress');
+Route::post('/progression',['as' => 'student.progression', 'uses' => 'DefaultController@storeProgress']);
 //Route::match(['post'],'/storeContent','EONConsulting\Storyline\Table\Http\Controllers\DefaultController@storeContent');
 
 });
