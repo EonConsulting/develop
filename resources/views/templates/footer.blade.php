58b5b0bca4970345b7197c931e0e3907863f9e45
Michael Hanekom <michael@vertopia.co.za>
2017-09-21 16:10:44 +0200

    Increment version numbering for release

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="main-footer">
     <div class="pull-right hidden-xs">
-        <strong>Version</strong> 2.3.11
+        <strong>Version</strong> 1.0.4
     </div>
     <strong>Copyright &copy; {{ date('Y') }} <a href="http://www.unisa.ac.za" target="_blank">University of South Africa</a>.</strong> All rights
     reserved.
