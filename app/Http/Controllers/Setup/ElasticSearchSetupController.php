281f92f1fc08d1551458dd98379b0e93ce62c9da
Kenny Crous <kcrous@iqbusiness.net>
2017-10-18 13:56:55 +0200

    ES Setup Wizard Started

---
+++ app/Http/Controllers/Setup/ElasticSearchSetupController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Setup;
+
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
+
+class ElasticSearchSetupController extends Controller {
+
+    public function index() {
+
+    }
+
+    public function create_new_index($settings = null, ) {
+        
+    }
+
+}
