389df9a0e3ecdc0af2e04e7bcc3b1c3ecb1ef374
Michael Hanekom <michael@vertopia.co.za>
2017-10-23 16:34:06 +0200

    Updates to sizing of widget switches

--- resources/views/dashboards/widgets/participants.blade.php
+++ resources/views/dashboards/widgets/participants.blade.php
@@ -3,8 +3,10 @@
         <div class="col-md-3 sp-top-15">
     <?php break; case "medium": ?>
         <div class="col-md-6 sp-top-15">
-    <?php break; case "large": default: ?>
-        <div class="col-md-12 sp-top-15">
+    <?php break; case "large": ?>
+        <div class="col-md-9 sp-top-15">
+    <?php break; case "xlarge": default: ?>
+        <div class="col-md-12 sp-top-15">            
     <?php break; endswitch; ?>
         <div class="dashboard-card shadow top-bdr-4">
 
@@ -15,9 +17,45 @@
             <div class="basic-clearfix">
                 <div class="progress-charts basic-clearfix">
                     <div class="container-fluid">
-
-                        <div class="container-fluid" id="student-assessment-container" style="height: 400px; padding:20px">
-                            <canvas id="student-assessment"></canvas>
+                        
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="nofitication-card shadow" style="margin-bottom: 10px">
+                                    <div class="notification-card-icon bg-col-1">
+                                        <i class="fa fa-group"></i>
+                                    </div>
+                                    <div class="notification-card-text">
+                                        <h1>Students</h1>
+                                        <p>725</p>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div class="col-md-6">
+                                <div class="nofitication-card shadow" style="margin-bottom: 10px">
+                                    <div class="notification-card-icon bg-col-2">
+                                        <i class="fa fa-bullhorn"></i>
+                                    </div>
+                                    <div class="notification-card-text">
+                                        <h1>Mentors</h1>
+                                        <p>25</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="nofitication-card shadow" style="margin-bottom: 10px">
+                                    <div class="notification-card-icon bg-col-3">
+                                        <i class="fa fa-user"></i>
+                                    </div>
+                                    <div class="notification-card-text">
+                                        <h1>Lecturers</h1>
+                                        <p>6</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
