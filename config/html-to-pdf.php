9ffcbe0d792060a3ba0760a7bf0fc4a934ab7ef0
Kyle Vorster <kyle@synergi.io>
2017-11-13 10:54:44 +0200

    Files forgotten during upgrade

---
+++ config/html-to-pdf.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'binary' => 'xvfb-run -- /usr/bin/wkhtmltopdf',
+    'no-outline',         // Make Chrome not complain
+    'margin-top'    => 10,
+    'margin-right'  => 10,
+    'margin-bottom' => 10,
+    'margin-left'   => 10,
+    'javascript-delay' => 2000,
+];
---
+++ database/migrations/2017_09_08_122525_create_metadata_store_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMetadataStoreTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('metadata_store', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->integer('metadata_type_id');
+            $table->string('description', 255);
+            $table->string('classification', 150)->nullable();
+            $table->integer('sequence')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('metadata_store');
+    }
+}
