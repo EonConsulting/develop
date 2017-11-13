2238cbc97e9e1e8dbf711328790641c9db95d0d9
Kyle Vorster <kyle@synergi.io>
2017-11-13 11:31:25 +0200

    Html to Pdf package

---
+++ packages/html-to-pdf/src/HtmlToPdfServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace EONConsulting\HtmlToPdf;
+
+use Illuminate\Support\ServiceProvider;
+
+class HtmlToPdfServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config/html-to-pdf.php' => config_path('html-to-pdf.php'),
+        ]);
+
+        $this->loadRoutesFrom(__DIR__.'/routes.php');
+
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'html-to-pdf');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/config/html-to-pdf.php', 'html-to-pdf'
+        );
+    }
+}
