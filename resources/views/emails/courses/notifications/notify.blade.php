9a647dcf154b89d399a923c9d0393b26e1634bca
Kyle Vorster <kyle@synergi.io>
2017-11-21 12:25:51 +0200

    added message box for custom message

--- resources/views/emails/courses/notifications/notify.blade.php
+++ resources/views/emails/courses/notifications/notify.blade.php
@@ -1,13 +1,8 @@
 @component('mail::message')
-# Introduction
-
-Email: {{ $user->email  }}
 
 Course: {{ $course->title }}
 
-@component('mail::button', ['url' => ''])
-Button Text
-@endcomponent
+{{ $message }}
 
 Thanks,<br>
 {{ config('app.name') }}
