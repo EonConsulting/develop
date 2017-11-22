4583e1090377c16108af038d555b136564943817
Michael Hanekom <michael@vertopia.co.za>
2017-11-22 12:32:16 +0200

    Added seeder for content_templates table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->command->info('metadata_type table seeded!');
         $this->call(MetadataStoreSeeder::class);
         $this->command->info('metadata_store table seeded!');
+        $this->call(ContentTemplatesSeeder::class);
+        $this->command->info('content_templates table seeded!');
     }
 }
