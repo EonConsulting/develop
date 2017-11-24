30e00d2cf7c1bdc2631f994245c30f29c497f28e
Michael Hanekom <michael@vertopia.co.za>
2017-11-24 16:41:18 +0200

    Added migration scripts for integration tables

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="main-footer">
     <div class="pull-right hidden-xs">
-        <strong>Version</strong> 2.0.5 beta
+        <strong>Version</strong> 2.0.6 beta
     </div>
     <strong>Copyright &copy; {{ date('Y') }} <a href="http://www.unisa.ac.za" target="_blank">University of South Africa</a>.</strong> All rights
     reserved.
