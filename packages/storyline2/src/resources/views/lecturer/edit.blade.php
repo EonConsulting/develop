c8b30246985b40766a6938eec02db61da13fd94c
Kenny Crous <kcrous@iqbusiness.net>
2017-12-08 13:02:37 +0200

    update preview modal

--- packages/storyline2/src/resources/views/lecturer/edit.blade.php
+++ packages/storyline2/src/resources/views/lecturer/edit.blade.php
@@ -557,7 +557,7 @@ Storyline Student Single
 </div>
 
 <div id="previewModal" class="modal fade" role="dialog">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-lg">
 
         <!-- Modal content-->
         <div class="modal-content content-body">
