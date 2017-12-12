baa48171213c44344ff525bd3fe2b11d6df58bd7
Kyle Vorster <kyle@synergi.io>
2017-12-12 09:56:30 +0200

    Package routes

---
+++ packages/tao-client/src/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+Route::group(['namespace' => '\EONConsulting\TaoClient\Http\Controllers'], function () {
+
+    Route::get('/tao-client/show', [
+        'as' => 'tao-client.show',
+        'uses' => 'TaoController@show'
+    ]);
+
+    Route::post('/tao-client/store', [
+        'as' => 'tao-client.store',
+        'uses' => 'TaoController@store'
+    ]);
+
+
+    Route::any('/tao-outcome/store', [
+        'as' => 'tao-outcome.store',
+        'uses' => 'OutcomeController@store'
+    ]);
+
+    Route::get('/tao-outcome/show', [
+        'as' => 'tao-outcome.show',
+        'uses' => 'OutcomeController@show'
+    ]);
+
+});
+
