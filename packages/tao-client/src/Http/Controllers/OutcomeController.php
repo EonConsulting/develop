92460ab13ed642373822c124d9b6f43e5e571dbe
Kyle Vorster <kyle@synergi.io>
2017-12-12 15:49:19 +0200

    Receive outcome from tao and show user a completed page

---
+++ packages/tao-client/src/Http/Controllers/OutcomeController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace EONConsulting\TaoClient\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use EONConsulting\TaoClient\Models\TaoResult;
+use EONConsulting\TaoClient\Services\TaoOutcome;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use EONConsulting\TaoClient\Exceptions\TaoOutcomeException;
+use EONConsulting\TaoClient\Jobs\TaoResultJob;
+use Auth;
+use Log;
+
+class OutcomeController extends Controller
+{
+
+    public function __construct()
+    {
+        Auth::loginUsingId(1400163);
+
+        \Debugbar::disable();
+    }
+
+    /**
+     * Store Tao outcome results
+     */
+    public function store()
+    {
+
+        try {
+
+            $outcome = TaoOutcome::handle();
+
+        } catch(TaoOutcomeException $e)
+        {
+            Log::debug($e->getMessage());
+            return response()->json(['status' => $e->getMessage()], 422);
+        }
+
+        if( ! $source_id = $outcome->getSourcedId())
+        {
+            Log::debug('TaoOutcome: Failed getting the source id!');
+            return response()->json(['status' => 'Failed'], 422);
+        }
+
+        try {
+
+            $result = TaoResult::bySourceId($source_id)->byPendingOutcome()->firstOrFail();
+
+        } catch(ModelNotFoundException $e)
+        {
+            Log::debug('TaoResult: | ' . $e->getMessage());
+            return response()->json(['status' => 'TaoResult: | ' . $e->getMessage()], 422);
+        }
+
+        $result->status = 1;
+        $result->status_message = 'Outcome results captured';
+        $result->score = $outcome->getResult();
+
+        $result->save();
+
+        TaoResultJob::dispatch($source_id);
+    }
+
+    /**
+     * Return page showing test was completed
+     *
+     * @return \Illuminate\View\View
+     */
+    public function show()
+    {
+        return view('tao-client::return');
+    }
+}
