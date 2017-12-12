95646e1b3ceeb3f28c689458891d5550d36cf370
Kyle Vorster <kyle@synergi.io>
2017-12-12 10:13:29 +0200

    Saving a storyline item will trigger observer to check the content if a tao iframe is embedded, update tao_assessments with storyline item id.

---
+++ packages/tao-client/src/Observers/StoryLineItemObserver.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace EONConsulting\TaoClient\Observers;
+
+use EONConsulting\TaoClient\Models\TaoAssessment;
+use EONConsulting\Storyline2\Models\StorylineItem;
+use EONConsulting\ContentBuilder\Models\Content;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Log;
+
+class StoryLineItemObserver
+{
+
+    /**
+     * Listen to the StorylineItem saved event.
+     *
+     * @param  \EONConsulting\Storyline2\Models\StorylineItem  $user
+     * @return void
+     */
+    public function saved(StorylineItem $storyline_item)
+    {
+
+        try {
+
+            $content = Content::findOrFail($storyline_item->content_id);
+
+        } catch(ModelNotFoundException $e)
+        {
+            Log::debug('StoryLineItemObserver: [Content] | ' . $e->getMessage());
+
+            return true;
+        }
+
+        if( ! $launch_url = $this->getLaunchUrl($content->body))
+        {
+            return true;
+        }
+
+        try {
+
+            $assessment = TaoAssessment::byLaunchUrl($launch_url)->firstOrFail();
+
+        } catch(ModelNotFoundException $e)
+        {
+            Log::debug('StoryLineItemObserver: [TaoAssessment] | ' . $e->getMessage());
+
+            return true;
+        }
+
+        $assessment->storyline_item_id = $storyline_item->id;
+
+        $assessment->save();
+
+        return true;
+    }
+
+    protected function getLaunchUrl($content)
+    {
+        preg_match("/src\=\"(.*?)\" /", $content, $match);
+
+        if( ! isset($match[1]))
+        {
+            return false;
+        }
+
+        $match = explode('?launch_url=', $match[1]);
+
+        if( ! isset($match[1]))
+        {
+            return false;
+        }
+
+        return $match[1];
+    }
+
+
+}
