e0bcb29bacdd9538a29880adcd86cdff8eecad35
Kyle Vorster <kyle@synergi.io>
2017-12-12 16:19:07 +0200

    Added config for tao database

--- config/database.php
+++ config/database.php
@@ -78,6 +78,21 @@ return [
             'prefix' => '',
         ],
 
+        'taodb' => [
+            'driver' => 'mysql',
+            'host' => env('TAODB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('TAODB_DATABASE', 'forge'),
+            'username' => env('TAODB_USERNAME', 'forge'),
+            'password' => env('TAODB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
     ],
 
     /*
