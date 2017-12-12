ec04e74584c619d5e02db0874eed1a8148b4905a
Kyle Vorster <kyle@synergi.io>
2017-12-12 09:56:14 +0200

    Package service provider.

---
+++ packages/tao-client/src/TaoClientServiceProvider.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace EONConsulting\TaoClient;
+
+use Illuminate\Support\ServiceProvider;
+use EONConsulting\TaoClient\Services\TaoApi;
+use EONConsulting\Storyline2\Models\StorylineItem;
+use EONConsulting\TaoClient\Observers\StoryLineItemObserver;
+
+class TaoClientServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/tao-client.php' => config_path('tao-client.php'),
+        ]);
+
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+
+
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'tao-client');
+
+        $this->registerCommands();
+        $this->registerObservers();
+        $this->registerRoutes();
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(TaoApi::class, function () {
+            $tao_api = new TaoApi(new \GuzzleHttp\Client(), $this->app['config']['tao-client']);
+            return $tao_api;
+        });
+    }
+
+    protected function registerCommands()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                \EONConsulting\TaoClient\Console\Commands\TaoClientCommand::class,
+            ]);
+        }
+    }
+
+    protected function registerObservers()
+    {
+        StorylineItem::observe(StoryLineItemObserver::class);
+    }
+
+    protected function registerRoutes()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+    }
+}
