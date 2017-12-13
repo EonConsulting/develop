d31296dbed0f2c46e13205922d6d357d2b941511
Kyle Vorster <kyle@synergi.io>
2017-12-13 14:13:44 +0200

    Fix old tao iframes

---
+++ packages/tao-client/src/Console/Commands/TaoFixIframesCommand.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace EONConsulting\TaoClient\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use EONConsulting\ContentBuilder\Models\Content;
+use EONConsulting\Storyline2\Models\StorylineItem;
+use EONConsulting\TaoClient\Models\TaoAssessment;
+use EONConsulting\TaoClient\Helpers;
+
+class TaoFixIframesCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'taoclient:fix-iframes';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        // $content = Content::whereIn('id', [89, 1080])->get();
+
+        $content = Content::all();
+
+
+        foreach($content as $text)
+        {
+
+            if( ! $launch_url = Helpers::getLaunchUrl($text->body))
+            {
+                $this->error('No Tao Found. ' . $text->title);
+                continue;
+            }
+
+            try {
+
+                $storyline_item = StorylineItem::where('content_id', $text->id)->firstOrFail();
+
+            } catch(ModelNotFoundException $e)
+            {
+                $this->error($e->getMessage());
+                continue;
+            }
+
+            $assessment = TaoAssessment::firstOrCreate([
+                'storyline_item_id' => $storyline_item->id,
+                'launch_url' => $launch_url,
+            ],[
+                'storyline_item_id' => $storyline_item->id,
+                'launch_url' => $launch_url,
+                'key' => config('tao-client.launch-options.key'),
+                'secret' => config('tao-client.launch-options.secret'),
+            ]);
+
+            $iframe = '<div class="iframeCover"><iframe allowtransparency="true" class="ckeditorframe" scrolling="yes" frameborder="0" src="' . route('tao-client.show') . '?launch_url=' . $launch_url . '" type="text/html" width="100%" height="800px"></iframe></div>';
+
+            $text->body = $iframe;
+
+            $text->save();
+
+            $this->info('Updated ' . $storyline_item->name);
+        }
+    }
+
+}
