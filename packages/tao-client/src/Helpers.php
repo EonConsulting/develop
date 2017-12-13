bec32f7954246d2f9f132d9e5475a6fa88d6dcbd
Kyle Vorster <kyle@synergi.io>
2017-12-13 14:10:34 +0200

    Helper class to work with tao launch url

---
+++ packages/tao-client/src/Helpers.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace EONConsulting\TaoClient;
+
+use DOMDocument;
+
+class Helpers
+{
+
+    static public function sanitizeTaoLaunchUrl($launch_url)
+    {
+        $parts = parse_url($launch_url);
+
+        if( ! array_key_exists('path', $parts))
+        {
+            return false;
+        }
+
+        $path = $parts['path'];
+
+        if(substr($path, 0, 1) != '/')
+        {
+            $path = '/' . $path;
+        }
+
+        $explode = explode('&height=', $path);
+
+        $path = $explode[0];
+
+        $tao_url = str_before(config('tao-client.api_url'), '/tao');
+
+        return $tao_url . trim($path);
+    }
+
+    static public function isTaoLink($url)
+    {
+        return ! preg_match('/DeliveryTool\/launch\//', $url);
+    }
+
+    static public function getLaunchUrl($content)
+    {
+        if( ! $tao_iframe_src = self::getTaoIframe($content))
+        {
+            return false;
+        }
+
+        if(self::isTaoLink($tao_iframe_src))
+        {
+            return false;
+        }
+
+        $launch_url = explode('?launch_url=', $tao_iframe_src);
+        $delivery_url = explode('?delivery_url=', $tao_iframe_src);
+
+        if(isset($launch_url[1]))
+        {
+            return self::sanitizeTaoLaunchUrl($launch_url[1]);
+        }
+
+        if(isset($delivery_url[1]))
+        {
+            return self::sanitizeTaoLaunchUrl($delivery_url[1]);
+        }
+
+        return false;
+    }
+
+    static public function getTaoIframe($content)
+    {
+
+        try {
+            $dom = new DOMDocument;
+
+            @$dom->loadHTML($content);
+
+            $iframes = $dom->getElementsByTagName('iframe');
+        } catch(\Exception $e)
+        {
+            return false;
+        }
+
+        foreach($iframes as $iframe)
+        {
+            return $iframe->getAttribute('src');
+        }
+
+        return false;
+    }
+
+}
