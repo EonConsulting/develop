eee2466f284859e28851d8b2571b1736c0850368
Kyle Vorster <kyle@synergi.io>
2017-12-13 14:11:16 +0200

    Disabled scrolling

--- packages/tao-client/src/resources/views/launch.blade.php
+++ packages/tao-client/src/resources/views/launch.blade.php
@@ -1,12 +1,13 @@
 <form action="{{ $url }}" name="ltiLaunchForm" id="ltiLaunchForm" method="post" target="basicltiLaunchFrame" encType="application/x-www-form-urlencoded">
-
     @foreach($inputs as $key => $value)
         <input type="hidden" name="{{ $key }}" value="{{ $value }}"/>
     @endforeach
 </form>
 
-<iframe class="lti_frameResize" name="basicltiLaunchFrame" id="basicltiLaunchFrame" src="" scrolling="yes"  frameborder="0" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%" transparency>
+<iframe class="lti_frameResize" name="basicltiLaunchFrame" id="basicltiLaunchFrame" src="" scrolling="no" frameborder="0" style="overflow:hidden;height:100%;width:100%" width="100%" height="800px" transparency>
     <p>frames_required</p>
 </iframe>
 
-<script>window.onload = function(){  document.forms['ltiLaunchForm'].submit()}</script>
+<script>
+    window.onload = function(){  document.forms['ltiLaunchForm'].submit()}
+</script>
