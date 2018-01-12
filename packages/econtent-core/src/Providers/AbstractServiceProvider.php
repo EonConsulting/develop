2fb977cd3a20c314d3677ab07652a088fcdce08a
Kyle Vorster <kyle@offsight.co.za>
2018-01-12 15:42:07 +0200

    Abstract service provider for packages with some helper methods in it

---
+++ packages/econtent-core/src/Providers/AbstractServiceProvider.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace EONConsulting\Core\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
+use Illuminate\Console\Application as Artisan;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Console\Command;
+use Symfony\Component\Finder\Finder;
+use ReflectionClass;
+
+abstract class AbstractServiceProvider extends ServiceProvider
+{
+
+    protected $namespace = '';
+
+    protected $middleware = [
+
+    ];
+
+    protected $route_prefix = '';
+
+    abstract protected function getPackageFolder();
+
+    /**
+     * Register all the commands by scanning the commands folder in the package
+     *
+     * @param $paths
+     */
+    protected function loadCommands($paths)
+    {
+        if ( ! $this->app->runningInConsole()) {
+            return;
+        }
+
+        $paths = array_unique(is_array($paths) ? $paths : (array) $paths);
+
+        $paths = array_filter($paths, function ($path) {
+            return is_dir($path);
+        });
+
+        if (empty($paths)) {
+            return;
+        }
+
+        foreach ((new Finder)->in($paths)->files() as $command)
+        {
+            $command = $this->namespace.str_replace(
+                    ['/', '.php'],
+                    ['\\', ''],
+                    Str::after($command->getPathname(), $this->getPackageFolder())
+                );
+
+            if (is_subclass_of($command, Command::class) && ! (new ReflectionClass($command))->isAbstract())
+            {
+                Artisan::starting(function ($artisan) use ($command) {
+                    $artisan->resolve($command);
+                });
+            }
+        }
+    }
+
+    /**
+     * Register the routes using the routes Facade
+     */
+    protected function registerRoutes()
+    {
+        Route::middleware($this->middleware)
+            ->namespace($this->namespace . '\Http\Controllers')
+            ->prefix($this->route_prefix)
+            ->group($this->getPackageFolder() .'/routes/web.php');
+    }
+
+}
