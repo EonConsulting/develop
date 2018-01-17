e52c795c00927e373d73fb6a1db378b1197c656a
Kyle Vorster <kyle@offsight.co.za>
2018-01-17 15:46:18 +0200

    Did some small refactoring and docblocks

--- packages/content-builder/src/Models/Category.php
+++ packages/content-builder/src/Models/Category.php
@@ -3,15 +3,41 @@
 namespace EONConsulting\ContentBuilder\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use EONConsulting\ContentBuilder\Models\Content;
 
 class Category extends Model {
 
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
     protected $table = 'lk_content_categories';
-    protected $primaryKey = 'id';
-    protected $fillable = ['name', 'tags'];
 
-    public function content() {
-        return $this->belongsToMany('EONConsulting\ContentBuilder\Models\Content', 'content_categories');
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'tags'
+    ];
+
+    /**
+     * Fetch the content for this model.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\belongsToMany
+     */
+    public function content()
+    {
+        return $this->belongsToMany(Content::class, 'content_categories');
     }
 
 }
