4a27450266f738680bbd1c5e9149f13fdcf728f4
Kyle Vorster <kyle@offsight.co.za>
2018-01-18 16:53:49 +0200

    Automattically add csrf tokens to ajax calls

--- resources/views/templates/default-scripts.blade.php
+++ resources/views/templates/default-scripts.blade.php
@@ -64,6 +64,12 @@
         }
         });
     });
+
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
 </script>
 
 <script src="https://unpkg.com/sweetalert2@7.0.7/dist/sweetalert2.all.js"></script>
