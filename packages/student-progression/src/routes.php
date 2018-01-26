b2f93bc7acf234ba19847a1fe971813f93479bde
reggiestain <reggiestain@gmail.com>
2018-01-26 14:36:33 +0200

    commented exports routes from student-progress pacakge

--- packages/student-progression/src/routes.php
+++ packages/student-progression/src/routes.php
@@ -3,8 +3,8 @@
 
 
 Route::group(['middleware' => ['web'], 'prefix' => 'student', 'namespace' => 'EONConsulting\Student\Progression\Http\Controllers'], function() {
-    Route::get('/module/downloadPDF/{course}',['as'=>'module.downloadPDF','uses'=>'DefaultController@downloadPDF']);
-    Route::get('/module/print/{id}',['as'=>'module.pdf-view','uses'=>'DefaultController@modulePDF']);
+    //Route::get('/module/downloadPDF/{course}',['as'=>'module.downloadPDF','uses'=>'DefaultController@downloadPDF']);
+    //Route::get('/module/print/{id}',['as'=>'module.pdf-view','uses'=>'DefaultController@modulePDF']);
     Route::group(['middleware' => ['auth', 'learner']], function() {
     Route::post('/progression','DefaultController@storeProgress')->name('student.progression');
     });
