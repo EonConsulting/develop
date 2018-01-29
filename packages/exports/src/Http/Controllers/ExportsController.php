874da8143ef24fe4871cc578bb8583fe9d375978
reggiestain <reggiestain@gmail.com>
2018-01-29 11:31:10 +0200

    wkhtmltopdf global config update 001

--- packages/exports/src/Http/Controllers/ExportsController.php
+++ packages/exports/src/Http/Controllers/ExportsController.php
@@ -24,27 +24,18 @@ class ExportsController extends Controller {
             ],
         ]);
 
-        $globalOptions = [
-        'no-outline', // Make Chrome not complain
-        'margin-top' => 10,
-        'margin-right' => 10,
-        'margin-bottom' => 10,
-        'margin-left' => 10,
-        'javascript-delay' => 2000,
-        // Explicitly tell wkhtmltopdf that we're using an X environment
-        'use-xserver',
-        // Enable built in Xvfb support in the command
-        'commandOptions' => array(
-        'enableXvfb' => true,
-        // Optional: Set your path to xvfb-run. Default is just 'xvfb-run'.
-        'xvfbRunBinary' => '/usr/bin/xvfb-run',
-        // Optional: Set options for xfvb-run. The following defaults are used.
-        'xvfbRunOptions' => '--auto-servernum',
-        ),
-        ];
+        $globalOptions = array(
+            'no-outline', // Make Chrome not complain
+            'margin-top' => 10,
+            'margin-right' => 10,
+            'margin-bottom' => 10,
+            'margin-left' => 10,
+            'javascript-delay' => 2000,
+        );
 
         $pdf->setOptions($globalOptions);
-        $binary = str_replace(array('\'', '"'), '', env('WKHTMLTOPDF_BIN'));
+        //$binary = str_replace(array('\'', '"'), '', env('WKHTMLTOPDF_BIN'));
+        $binary = storage_path() . '/wkhtmltopdf/bin/wkhtmltopdf.exe';
         $pdf->binary = $binary;
 
         return $pdf;
