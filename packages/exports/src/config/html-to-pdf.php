9233ba71e61712d200bb7e565009d7c7780223a6
reggiestain <reggiestain@gmail.com>
2018-01-29 14:53:06 +0200

    wkhtmltopdf global config update with ignore warnings

--- packages/exports/src/config/html-to-pdf.php
+++ packages/exports/src/config/html-to-pdf.php
@@ -2,14 +2,14 @@
 
 return [
     //'binary' => '/usr/bin/wkhtmltopdf',
-    'no-outline',         // Make Chrome not complain
+    //'no-outline',         // Make Chrome not complain
     'margin-top'    => 10,
     'margin-right'  => 10,
     'margin-bottom' => 10,
     'margin-left'   => 10,
     'javascript-delay' => 2000,
     // Explicitly tell wkhtmltopdf that we're using an X environment
-    'use-xserver',
+    //'use-xserver',
     'ignoreWarnings' => true,
 
     // Enable built in Xvfb support in the command
