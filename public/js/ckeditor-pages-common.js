a96571046e4ea33a1c1db2c292d9c6a4a1c73ee8
Kenny Crous <kcrous@iqbusiness.net>
2018-02-19 10:47:04 +0200

    ck-editor update

--- public/js/ckeditor-pages-common.js
+++ public/js/ckeditor-pages-common.js
@@ -5,7 +5,6 @@ function init_editor(editor_id){
     editor = CKEDITOR.replace(editor_id, {
         scayt_autoStartup: true,
         extraPlugins: 'interactivegraphs,taoclient,ltieditorv2,mathjax,dialog,xml,templates,widget,lineutils,widgetselection,clipboard',
-        removePlugins: 'sourcearea',
         allowedContent: true,
         fullPage: false,
         mathJaxLib: '//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_SVG'
