436c6da95b8a04ec74db8a05ce9ecf70bda083f7
Kenny Crous <kcrous@iqbusiness.net>
2018-02-23 13:09:40 +0200

    fixed stylesheet_url bug

--- public/js/ckeditor-pages-common.js
+++ public/js/ckeditor-pages-common.js
@@ -3,9 +3,9 @@ function init_editor(editor_id){
     var editor = {};
 
     editor = CKEDITOR.replace(editor_id, {
-        contentsCss : stylesheet_url,
+        //contentsCss : stylesheet_url,
         removePlugins: 'scayt,wsc',
-	disableNativeSpellChecker: false,
+	    disableNativeSpellChecker: false,
         scayt_autoStartup: true,
         extraPlugins: 'interactivegraphs,taoclient,ltieditorv2,mathjax,dialog,xml,templates,widget,lineutils,widgetselection,clipboard',
         allowedContent: true,
