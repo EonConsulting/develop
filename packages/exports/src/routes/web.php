88ea9461ec13eb8baebeda5d3afcfb068cbcdd7d
Kyle Vorster <kyle@synergi.io>
2018-02-27 17:20:28 +0200

    New routes for full course downloads

--- packages/exports/src/routes/web.php
+++ packages/exports/src/routes/web.php
@@ -1,5 +1,30 @@
 <?php
 
+Route::middleware(['role:instructor'])->group(function () {
+
+    Route::get('/course/full-html-export/{course}', [
+        'as' => 'export.full-html-export',
+        'uses' => 'ExportCourseHtmlController@generate'
+    ]);
+
+
+});
+
+Route::middleware(['role:instructor|learner'])->group(function () {
+
+    Route::get('/course/full-html-download/{course}', [
+        'as'=>'export.full-html-download',
+        'uses'=>'DownloadCourseHtmlController@show'
+    ]);
+
+
+});
+
+
+
+
+
+
 //Route::post('/html-to-pdf', ['as' => 'html-to-pdf.store','uses' => 'ConvertController@store']);
 Route::get('/module/downloadPDF/{course}',['as'=>'module.downloadPDF','uses'=>'ExportsController@modulePDF']);
 Route::get('/module/print/{course}',['as'=>'module.pdf-view','uses'=>'ExportsController@downloadPDF']);
