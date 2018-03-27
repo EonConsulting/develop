26b65c62bf91b5b5560edf7a53aabeec925ab8fd
Kyle Vorster <kyle@synergi.io>
2018-03-27 17:29:55 +0200

    Forgot to update the roles

--- packages/tao-client/src/routes/web.php
+++ packages/tao-client/src/routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-Route::get('/tao-client/show', ['middleware' => ['web', 'auth'],
+Route::get('/tao-client/show', ['middleware' => ['web', 'auth', 'role:instructor|learner'],
     'as' => 'tao-client.show',
     'uses' => 'TaoController@show'
 ]);
 
-Route::post('/tao-client/store', ['middleware' => ['web', 'auth'],
+Route::post('/tao-client/store', ['middleware' => ['web', 'auth', 'role:instructor'],
     'as' => 'tao-client.store',
     'uses' => 'TaoController@store'
 ]);
@@ -15,7 +15,7 @@ Route::any('/tao-outcome/store', ['middleware' => [\EONConsulting\TaoClient\Http
     'uses' => 'OutcomeController@store'
 ]);
 
-Route::get('/tao-outcome/show', ['middleware' => ['web', 'auth'],
+Route::get('/tao-outcome/show', ['middleware' => ['web', 'auth', 'role:instructor|learner'],
     'as' => 'tao-outcome.show',
     'uses' => 'OutcomeController@show'
 ]);
