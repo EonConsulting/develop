03687a2b8760c8d0c6cf4cba40d57e54c8de4fd4
Kyle Vorster <kyle@synergi.io>
2018-04-11 11:07:46 +0200

    Not needed at the moment

--- packages/content-builder/src/resources/views/content/view.blade.php
+++ packages/content-builder/src/resources/views/content/view.blade.php
@@ -16,7 +16,6 @@
 
         <div class="content-page shadow">
 
-            <a href="#" class="pull-right" style="font-size: 16px; margin-left: 15px;"><i class="fa fa-trash"></i><span class="hidden-xs"> Delete</span></a>
             <a href="{{ url('content/update/'.$content->id) }}" class="pull-right" style="font-size: 16px; margin-left: 15px;"><i class="fa fa-pencil"></i><span class="hidden-xs"> Edit</span></a>
             
             <h3 style="margin: 0px;"><?php echo $content->title; ?></h3>
