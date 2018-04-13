fab6fa7a50b462530031e087f22904792316c053
Michael Hanekom <michael@vertopia.co.za>
2018-04-13 10:52:08 +0200

    - Touched-up the login page - Incremented the deployment version to 2.0.21 beta

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,10 +2,16 @@
 
 @section('content')
     <div class="container">
+        <div class="row" style="padding-bottom: 20px">
+            <div class="col-md-8 col-md-offset-2">
+                <img src="/img/unisa-logo.png" alt="unisa-logo" width="50%">
+            </div>
+        </div>
+        
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Login</div>
+                    <div class="panel-heading">e-Content System Login</div>
 
                     <div class="panel-body">
                         <form class="form-horizontal" method="POST" action="{{ route('login') }}">
