4cf410b95d8c9e74d44d18fe9a485241cd9ea7be
Kyle Vorster <kyle@synergi.io>
2018-04-18 16:01:17 +0200

    Export Controllers and Jobs

---
+++ packages/exports/src/Http/Controllers/ExportCourseHtmlController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace EONConsulting\Exports\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use EONConsulting\Storyline2\Models\Course;
+use EONConsulting\Exports\Jobs\CourseExportJob;
+use Illuminate\Support\Facades\Auth;
+
+class ExportCourseHtmlController extends Controller
+{
+
+    /**
+     * Generate a full course html export
+     *
+     * @param \EONConsulting\Storyline2\Models\Course $course
+     */
+    public function generate(Course $course)
+    {
+        CourseExportJob::dispatch($course);
+
+        $message = 'Export started, file will be available for download soon.';
+
+        return redirect()->route('courses', [])->with('flash.success', $message);
+    }
+
+}
